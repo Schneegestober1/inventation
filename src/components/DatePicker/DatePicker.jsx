3088d509244cdc456471611dde1e28c75af99a7d
Kholov Rustam <rustamkholow5@gmail.com>
2025-03-31 22:24:26 +0300

    some cganges were made

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import styles from '../DatePicker/date-picker.module.scss';
-
-
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "../DatePicker/date-picker.module.scss";
 
 function DatePicker() {
-  const [onChange] = useState(new Date());
-   const defaultDate = new Date(2025, 7, 15);
+  const defaultDate = new Date(2025, 7, 15);
+  const monthName = defaultDate.toLocaleString("ru-RU", { month: "long" }).toUpperCase();
+  const year = defaultDate.getFullYear();
 
   return (
     <div className={styles.container}>
-      <Calendar 
-      className={styles.customCalendar}
-      onChange={onChange} 
-      prevLabel={null}  
-      prev2Label={null} 
-      nextLabel={null}  
-      next2Label={null}
-      value={defaultDate}
-       />
+      
+      <Calendar
+        className={styles.customCalendar}
+        prevLabel={null}
+        prev2Label={null}
+        nextLabel={null}
+        next2Label={null}
+        value={defaultDate}
+        navigationLabel={() => <div className={styles.dateHeader}>{monthName}<span className={styles.year}>{year}</span></div>} 
+      />
     </div>
   );
 }
 
-export default DatePicker
+export default DatePicker;
